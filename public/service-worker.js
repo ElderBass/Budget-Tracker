f6c56b3f33d94b853e6002022945d8172b87a4b9
Seth Zyg <zygster11@gmail.com>
2021-02-25 22:09:33 -0600

    Service worker js file not working properly for static cache. Not sure why.

--- public/service-worker.js
+++ public/service-worker.js
@@ -13,11 +13,17 @@ const FILES_TO_CACHE = [
 
 //this caches all of our files from 'FILE_TO_CACHE' upon opening the application(???)
 self.addEventListener("install", (event) => {
+    event.waitUntil(
+      caches
+        .open(STATIC_CACHE)
+        .then((cache) => cache.addAll(FILES_TO_CACHE))
+        .then(() => self.skipWaiting())
+    );
+//   event.waitUntil(
+//     caches.open(STATIC_CACHE).then((cache) => cache.addAll(FILES_TO_CACHE))
+//   );
   event.waitUntil(
-    caches
-      .open(STATIC_CACHE)
-      .then((cache) => cache.addAll(FILES_TO_CACHE))
-      .then(() => self.skipWaiting())
+    caches.open(DATA_CACHE).then((cache) => cache.add("/api/transaction"))
   );
 });
 
@@ -44,49 +50,54 @@ self.addEventListener("activate", (event) => {
   );
 });
 
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.open(STATIC_CACHE).then((cache) => {
+      return cache.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      });
+    })
+  );
+  // non GET requests are not cached and requests to other origins are not cached
+  if (
+    event.request.method !== "GET" ||
+    !event.request.url.startsWith(self.location.origin)
+  ) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
 
-
-self.addEventListener("fetch", event => {
-    // non GET requests are not cached and requests to other origins are not cached
-    if (
-      event.request.method !== "GET" ||
-      !event.request.url.startsWith(self.location.origin)
-    ) {
-      event.respondWith(fetch(event.request));
-      return;
-    }
-  
-    // handle runtime GET requests for data from /api routes
-    if (event.request.url.includes("/api/transaction")) {
-      // make network request and fallback to cache if network request fails (offline)
-      event.respondWith(
-        caches.open(DATA_CACHE).then(cache => {
-          return fetch(event.request)
-            .then(response => {
-              cache.put(event.request, response.clone());
-              return response;
-            })
-            .catch(() => caches.match(event.request));
-        })
-      );
-      return;
-    }
-  
-    // use cache first for all other requests for performance
+  // handle runtime GET requests for data from /api routes
+  if (event.request.url.includes("/api/transaction")) {
+    // make network request and fallback to cache if network request fails (offline)
     event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-  
-        // request is not in cache. make network request and cache the response
-        return caches.open(DATA_CACHE).then(cache => {
-          return fetch(event.request).then(response => {
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
+      caches.open(DATA_CACHE).then((cache) => {
+        return fetch(event.request)
+          .then((response) => {
+            cache.put(event.request, response.clone());
+            return response;
+          })
+          .catch(() => caches.match(event.request));
       })
     );
-  });
+    return;
+  }
+
+  // use cache first for all other requests for performance
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      // request is not in cache. make network request and cache the response
+      return caches.open(DATA_CACHE).then((cache) => {
+        return fetch(event.request).then((response) => {
+          return cache.put(event.request, response.clone()).then(() => {
+            return response;
+          });
+        });
+      });
+    })
+  );
+});
